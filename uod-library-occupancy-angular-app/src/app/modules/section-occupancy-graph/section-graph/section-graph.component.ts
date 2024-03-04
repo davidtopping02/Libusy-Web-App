549b2e7b4ae56b4bfcea1375626bf871c119f4e4
David Topping <71976336+davidtopping02@users.noreply.github.com>
2024-03-04 12:01:01 +0000

    updated times to 12 on the graph components

--- uod-library-occupancy-angular-app/src/app/modules/section-occupancy-graph/section-graph/section-graph.component.ts
+++ uod-library-occupancy-angular-app/src/app/modules/section-occupancy-graph/section-graph/section-graph.component.ts
@@ -23,38 +23,36 @@ export class SectionGraphComponent implements OnInit {
     const currentHour = new Date().getHours();
     this.dayArray = Array.from({ length: 12 }, (_, i) => {
       const hour = (currentHour - 2 + i + 24) % 24;
-      // Skip hour 24 (which is hour 0 in 24-hour format)
       if (hour === 0) {
         return null; // Skip this hour
       }
-      const isCurrent = currentHour === hour; // Check if this hour is the current hour
+      const isCurrent = currentHour === hour;
 
-      // Represent the hour in 24-hour format
-      const displayHour = hour;
-      const hourFormatted = `${displayHour}`.padStart(2, '0'); // Pad single-digit hours with leading zero
+      // Convert 24-hour time to 12-hour format and add AM or PM
+      const displayHour = hour % 12 === 0 ? 12 : hour % 12;
+      const amPm = hour < 12 || hour === 24 ? 'am' : 'pm';
+      const hourFormatted = `${displayHour}${amPm}`.padStart(2, '0'); // Padding might not be necessary due to string suffix
 
-      let value = 10;
-      // Check if there's data available for this hour in sectionData
+      let value = 10; // Default value
       if (this.sectionData && this.sectionData.hours) {
         const hourData = this.sectionData.hours.find((data: any) => {
-          // Extract only the hour part from the time (e.g., "16" from "16:00")
-          const dataHour = Number(data.time.split(':')[0]);
+          const dataHour = Number(data.time.split(':')[0]) % 24;
           return dataHour === hour;
         });
-        // If data available, use its value
         if (hourData) {
-          value = hourData.occupancy_percentage;
+          // Check if occupancy_percentage is greater than 100
+          value = hourData.occupancy_percentage > 100 ? 100 : hourData.occupancy_percentage;
         }
       }
-
-      // Format the time with the hour only, keeping it in 24-hour format
       return {
         time: hourFormatted,
         value: value,
         current: isCurrent
       } as HourTemplate | null;
-    }).filter((hour): hour is HourTemplate => hour !== null); // Filter out null entries and adjust the typing
+    }).filter((hour): hour is HourTemplate => hour !== null); // Filter out null entries
   }
+
+
 
 
 
